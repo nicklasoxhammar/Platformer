9c9fa02f90a878b38ce0cbb142aef6c88365d332
nicklasoxhammar <nicklas.oxhammar@hotmail.com>
2018-09-02 19:18:15 +0200

    initial commit

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    [SerializeField] GameObject player;
+    [SerializeField] float moveCameraAt = 7.0f;
+
+    float zPos;
+
+
+    void Start () {
+
+        zPos = transform.position.z;
+		
+	}
+
+	void Update () {
+
+        if (player.transform.position.x > transform.position.x + moveCameraAt) {
+
+            transform.position = transform.position = new Vector3(player.transform.position.x - moveCameraAt, transform.position.y, zPos);
+        }
+
+        if (player.transform.position.x < transform.position.x - moveCameraAt) {
+
+            transform.position = transform.position = new Vector3(player.transform.position.x + moveCameraAt, transform.position.y, zPos);
+        }
+
+        if (player.transform.position.y > transform.position.y + moveCameraAt) {
+
+            transform.position = transform.position = new Vector3(transform.position.x, player.transform.position.y - moveCameraAt, zPos);
+        }
+
+        if (player.transform.position.y < transform.position.y - moveCameraAt) {
+
+            transform.position = transform.position = new Vector3(transform.position.x, player.transform.position.y + moveCameraAt, zPos);
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+    public float speed = 15.0f;
+    public float jumpForce = 1700.0f;
+
+    bool isGrounded = true;
+
+	void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+		
+	}
+
+	void FixedUpdate () {
+
+        Move();
+		
+	}
+
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+
+            isGrounded = false;
+
+            rb.AddForce(new Vector2(0.0f, jumpForce));
+        }
+
+    }
+
+    void Move() {
+
+        float direction = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(direction * speed, rb.velocity.y);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+
+        if (collision.gameObject.tag == "Ground") { 
+        isGrounded = true;
+        }
+    }
+}
