adf74e9728a3f039d9b643ca0ae2895ea6fb6f58
nicklasoxhammar <nicklas.oxhammar@hotmail.com>
2018-09-11 12:53:24 +0200

    Started working on bee and enemy gameobjects

---
+++ Assets/Bee.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bee : MonoBehaviour {
+
+    [SerializeField] float speed = 5.0f;
+    [SerializeField] float distance = 10.0f;
+    [SerializeField] bool upAndDown = false;
+
+    private GameObject target = null;
+    private Vector3 offset;
+
+    float direction = 1;
+
+    Vector3 scale;
+
+    float startingXPos;
+    float startingYPos;
+
+    void Start() {
+
+        startingXPos = transform.position.x;
+        startingYPos = transform.position.y;
+
+        scale = transform.localScale;
+    }
+
+    void Update() {
+
+        Vector3 position = transform.position;
+
+        if (upAndDown) {
+
+            position.y += speed * direction * Time.deltaTime;
+
+            if (position.y >= startingYPos + distance || position.y <= startingYPos - distance) {
+                ChangeDirection();
+            }
+
+        }
+
+        if (upAndDown == false) {
+
+            position.x += speed * direction * Time.deltaTime;
+
+            if (position.x >= startingXPos + distance || position.x <= startingXPos - distance) {
+                ChangeDirection();
+            }
+        }
+
+        transform.position = position;
+
+    }
+
+
+    void OnCollisionStay2D(Collision2D col) {
+        target = col.gameObject;
+        offset = target.transform.position - transform.position;
+    }
+    void OnCollisionExit2D(Collision2D col) {
+        target = null;
+    }
+
+    void LateUpdate() {
+        if (target != null) {
+            target.transform.position = transform.position + offset;
+        }
+
+    }
+
+
+    void ChangeDirection() {
+
+        direction *= -1;
+        transform.localScale = new Vector3(scale.x * direction, scale.y, scale.z);
+
+    }
+
+}
---
+++ Assets/Enemy.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] float speed = 5.0f;
+    [SerializeField] float distance = 10.0f;
+
+    float direction = 1;
+
+    float startingXPos;
+
+	void Start () {
+
+        startingXPos = transform.position.x;
+	}
+
+	void Update () {
+
+        Vector3 position = transform.position;
+        position.x += speed * direction * Time.deltaTime;
+        transform.position = position;
+
+
+        if (position.x >= startingXPos + distance || position.x <= startingXPos - distance) {
+            ChangeDirection();
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        
+        if(collision.gameObject.tag == "Player") {
+            Debug.Log("DIE!");
+
+            PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+
+            if (player.isDashing) {
+                Debug.Log("kill enemy");
+                Die();
+            }
+            else {
+                player.Die();
+            }
+        }
+    }
+
+    void ChangeDirection() {
+
+        direction *= -1;
+
+    }
+
+    void Die() {
+
+        Destroy(gameObject);
+    }
+
+
+}
