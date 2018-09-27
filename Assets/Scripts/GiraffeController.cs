a175cf4d0b7d196185fb6c62ed9e62d21f3a8569
FannyHogberg <fanhog@gmail.com>
2018-09-27 15:13:47 +0200

    state Machine

---
+++ Assets/Scripts/GiraffeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+
+public class GiraffeController : MonoBehaviour {
+
+    private Animator animator;
+
+    int goToIdleStateHash = Animator.StringToHash("GoToIdle");
+
+	// Use this for initialization
+	void Start () {
+
+        animator = GetComponentInParent<Animator>();
+        if (animator == null)
+        {
+            Debug.Log("HEJKLAJSKLJSDKLAJDKALSDJKSLADJKLASD");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        animator.SetTrigger(goToIdleStateHash);
+    }
+
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Player")
+    //    {
+    //        animator.SetTrigger(goToIdleStateHash);
+    //    }
+    //}
+
+}
