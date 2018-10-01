36d9e4d54a29bf8e80b3f27fbdf481d4d934f79f
FannyHogberg <fanhog@gmail.com>
2018-10-01 11:59:29 +0200

    new enemy

--- Assets/Scripts/GiraffeController.cs
+++ Assets/Scripts/GiraffeController.cs
@@ -6,16 +6,18 @@ using Spine.Unity;
 public class GiraffeController : MonoBehaviour {
 
     private Animator animator;
-
+    [SerializeField] Collider2D giraffeBack;
 
 	// Use this for initialization
 	void Start () {
-
+        
+        animator = GetComponentInParent<Animator>();
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 
 
 	}
