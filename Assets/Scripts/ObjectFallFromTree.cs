88a28b041fb59785fe4f4feb77eb6ae96bab62c9
FannyHogberg <fanhog@gmail.com>
2018-10-09 12:13:23 +0200

    Tree

--- Assets/Scripts/ObjectFallFromTree.cs
+++ Assets/Scripts/ObjectFallFromTree.cs
@@ -2,28 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectFallFromTree : MonoBehaviour {
+public class ObjectFallFromTree : MonoBehaviour
+{
 
     Rigidbody2D rg;
-	// Use this for initialization
-	void Start () {
-        rg = GetComponent<Rigidbody2D>();
-        rg.gravityScale = 0;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-
-
-
-    public void ShakeAndFall()
+    // Use this for initialization
+    void Start()
     {
-        LeanTween.rotateY(gameObject, 10f, 1f).setEaseShake().setOnComplete(() => {
-            rg.gravityScale = 1;
+        rg = GetComponent<Rigidbody2D>();
+        //rg.isKinematic = true;
+        rg.gravityScale = 0;
+        rg.velocity = Vector3.zero;
+    }
+
+
+    public void ShakeAndFall(float time)
+    {
+        float animationTime = time * 0.5f;
+
+        LeanTween.rotateZ(gameObject, 10f, animationTime).setEaseShake().setRepeat(2).setLoopPingPong().setOnComplete(() =>
+        {
+            LeanTween.rotateZ(gameObject, -10f, animationTime).setEaseOutBack().setOnComplete(() =>
+            {
+                //rg.isKinematic = false;
+                rg.gravityScale = 1;
+
+            });
         });
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            rg.velocity = Vector2.zero;
+        }
     }
 
 }
--- Assets/Scripts/TreeController.cs
+++ Assets/Scripts/TreeController.cs
@@ -7,60 +7,83 @@ using Cinemachine;
 
 public class TreeController : MonoBehaviour
 {
-
-    [SerializeField] CinemachineVirtualCamera virualCamBig;
-    [SerializeField] CinemachineVirtualCamera virualCamObjectInTree;
-    [SerializeField] float waitSecBeforeOriginalCamera = 2f;
+    [SerializeField] GameObject objectsToPutInTreePrefab;
+    [SerializeField] int numberOfObjects = 2;
+    [SerializeField] int sizeOfObjectArea = 8;
+    [SerializeField] float offsetYArea = 4f;
+    [SerializeField] CinemachineVirtualCamera VRCamBig;
+    [SerializeField] CinemachineVirtualCamera VRCamObjectInTree;
     private ObjectFallFromTree objectInTree;
     private bool isMoving;
 
+    private List<ObjectFallFromTree> listOfObjects = new List<ObjectFallFromTree>();
+
+    CinemachineTargetGroup targetGroup;
     // Use this for initialization
     void Start()
     {
-        objectInTree = GetComponentInChildren<ObjectFallFromTree>();
-        //virualCamBig = GetComponentInChildren<CinemachineVirtualCamera>();
+        targetGroup = GameObject.Find("TargetGroup2").GetComponent<CinemachineTargetGroup>();
+
+        List<CinemachineTargetGroup.Target> targets = new List<CinemachineTargetGroup.Target>();
+
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            ObjectFallFromTree newObject = Instantiate(objectsToPutInTreePrefab, transform).GetComponent<ObjectFallFromTree>();
+            Vector2 position = new Vector2(newObject.transform.position.x, newObject.transform.position.y + offsetYArea);
+            newObject.transform.position = position; 
+            newObject.transform.position += Random.insideUnitSphere * sizeOfObjectArea;
+            listOfObjects.Add(newObject);
+            CinemachineTargetGroup.Target targetObject = new CinemachineTargetGroup.Target();
+            targetObject.target = newObject.transform;
+            targetObject.radius = 2f;
+            targetObject.weight = 1;
+            targets.Add(targetObject);
+        }
+        targetGroup.m_Targets = targets.ToArray();
+
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
 
-    }
-
+    //Start animation and shake things from tree
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.tag == "Player" && CrossPlatformInputManager.GetButtonDown("Dash") && !isMoving)
         {
             isMoving = true;
-            virualCamBig.enabled = true;
+            VRCamBig.enabled = true;
             LeanTween.rotateZ(transform.parent.gameObject, 4f, 1f).setEaseInBack().setOnComplete(() =>
             {
+                ShakeAndFallObjects();
                 LeanTween.rotateZ(transform.parent.gameObject, -4f, 0.2f).setRepeat(5).setLoopPingPong()
                          .setOnComplete(() =>
                          {
+                             VRCamBig.enabled = false;
+                             VRCamObjectInTree.enabled = true;
                              LeanTween.rotateZ(transform.parent.gameObject, 0f, 1f).setEaseOutBounce()
                                       .setOnComplete(() =>
                                       {
-
                                           isMoving = false;
-                                          virualCamBig.enabled = false;
-                                          virualCamObjectInTree.enabled = true;
-                                          objectInTree.ShakeAndFall();
-                                          StartCoroutine(EnableOriginalCamera());
+                                          StartCoroutine(EnableOriginalCamera(1.5f));
                                       });
                          });
             });
         }
+    }
 
+    private void ShakeAndFallObjects()
+    {
+        foreach(ObjectFallFromTree thing in listOfObjects)
+        {
+            thing.ShakeAndFall(2f);
+        }
     }
 
 
-
-
-    IEnumerator EnableOriginalCamera()
+    IEnumerator EnableOriginalCamera(float delaySec)
     {
-        yield return new WaitForSeconds(waitSecBeforeOriginalCamera);
-        virualCamObjectInTree.enabled = false;
+        yield return new WaitForSeconds(delaySec);
+        VRCamObjectInTree.enabled = false;
         isMoving = false;
     }
 
