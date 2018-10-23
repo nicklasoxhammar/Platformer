4a3aa2467b63bce35524e35cbad0a25590209797
FannyHogberg <fanhog@gmail.com>
2018-10-23 22:17:58 +0200

    Start text

---
+++ Assets/Scripts/EldaAnimationStartScene.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+
+public class EldaAnimationStartScene : MonoBehaviour {
+
+    private string fallingAnimationName = "FALLING";
+    private string jumpAnimationName = "JUMP";
+    private string idleAnimationName = "STANDING";
+    private string runAnimationName = "RUN";
+    public Transform movingPoints;
+    public Transform startPosBeforeSkate;
+    public Transform endPosSkate;
+    private SkeletonAnimation skeletonAnimation;
+    private Rigidbody2D rb;
+    private int moveCounter = 0;
+    private float speed = 0.15f;
+    public Transform skateboard;
+
+    // Use this for initialization
+    void Start()
+    {
+        skeletonAnimation = GetComponent<SkeletonAnimation>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShowRightAnimation();
+    }
+
+    private void MoveAndJump()
+    {
+        if (moveCounter < movingPoints.childCount)
+        {
+            float distance = Vector2.Distance(transform.position, movingPoints.GetChild(moveCounter).transform.position);
+            float time = distance / speed * Time.deltaTime;
+            LeanTween.moveX(gameObject, movingPoints.GetChild(moveCounter).transform.position.x, time).setOnComplete(() =>
+            {
+                //jump;
+                rb.AddForce(new Vector2(150, 350));
+                if (moveCounter == movingPoints.childCount - 1)
+                {
+                    skateboard.position = startPosBeforeSkate.position;
+                    skateboard.gameObject.SetActive(true);
+                    //rb.isKinematic = true;
+                    //Invoke("startSkate", 1f);
+                }
+            });
+            moveCounter++;
+        }
+    }
+
+    private void startSkate()
+    {
+        gameObject.transform.position = startPosBeforeSkate.position;
+        LeanTween.moveX(gameObject, endPosSkate.position.x, 1f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        MoveAndJump();
+    }
+
+    private void ShowRightAnimation()
+    {
+        if (rb.velocity.y < 0)
+        {
+            skeletonAnimation.AnimationName = fallingAnimationName;
+
+        }
+        else if (rb.velocity.y > 0)
+        {
+            skeletonAnimation.AnimationName = jumpAnimationName;
+        }
+        else
+        {
+            skeletonAnimation.AnimationName = runAnimationName;
+        }
+    }
+}
---
+++ Assets/Scripts/StartSceneAnimation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+
+public class StartSceneAnimation : MonoBehaviour {
+
+    private string fallingAnimationName = "FALLING";
+    private string jumpAnimationName = "JUMP";
+    private string idleAnimationName = "STANDING";
+    private string runAnimationName = "RUN";
+    public GameObject elda;
+    public Transform movingPoints;
+    private SkeletonAnimation skeletonAnimationElda;
+
+	// Use this for initialization
+	void Start () {
+        skeletonAnimationElda = elda.GetComponent<SkeletonAnimation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void MoveToRight()
+    {
+        Debug.Log("MOVE");
+        LeanTween.moveX(elda, movingPoints.GetChild(1).transform.position.x, 2f);
+        skeletonAnimationElda.AnimationName = runAnimationName;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        MoveToRight();
+    }
+}
